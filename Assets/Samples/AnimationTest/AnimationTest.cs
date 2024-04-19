f0102c968d392f30f8675b93a3e5cdc9107beb24
Arnilsen Arthur <arnilsenarthur@gmail.com>
2024-04-19 19:26:18 -0300

    Code Clean up

--- Assets/Samples/AnimationTest/AnimationTest.cs
+++ Assets/Samples/AnimationTest/AnimationTest.cs
@@ -8,20 +8,19 @@ namespace Samples.AnimationTest
         private static readonly int _Walking = Animator.StringToHash("walking");
         
         public Animator animator;
-        public bool waving = false;
+        public bool waving;
         
         public void Update()
         {
             if (!HasAuthority)
                 return;
+
+            if (Input.GetKeyDown(KeyCode.A))
+                waving = !waving;
+            
             var v = Mathf.Lerp(animator.GetFloat(_Walking), Input.GetAxisRaw("Vertical"), Time.deltaTime * 3);
             animator.SetFloat(_Walking, v);
 
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                waving = !waving;
-            }
-            
             var w = Mathf.Lerp(animator.GetLayerWeight(1), waving ? 1 : 0, Time.deltaTime * 3f);
             animator.SetLayerWeight(1, w);
         }
