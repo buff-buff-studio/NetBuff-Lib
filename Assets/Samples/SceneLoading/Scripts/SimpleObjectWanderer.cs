459fab61bb6e4700d316ae1639ace55e2a662870
Arnilsen Arthur Castilho Lopes <arnilsenarthur@gmail.com>
2025-06-10 15:08:48 -0300

    Heavy Test

---
+++ Assets/Samples/SceneLoading/Scripts/SimpleObjectWanderer.cs
@@ -0,0 +1,32 @@
+﻿using NetBuff.Components;
+using UnityEngine;
+
+namespace Samples.SceneLoading
+{
+    public class SimpleObjectWanderer : NetworkBehaviour
+    {
+        private Vector3 _startPos;
+        private Vector3 _position;
+        //private float maxDistance = 2f;
+
+        private void Start()
+        {
+            if (!HasAuthority)
+                return;
+
+            _startPos = transform.position;
+            _position = transform.position;
+        }
+
+        private void Update()
+        {
+            if (!HasAuthority)
+                return;
+
+            if (Vector3.Distance(transform.position, _position) < 0.2f)
+                _position = Random.insideUnitSphere * 2f + _startPos;
+                
+            transform.position = Vector3.MoveTowards(transform.position, _position, Time.deltaTime * 2f);
+        }
+    }
+}
